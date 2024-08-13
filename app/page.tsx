97499ad9736c5fd3b25c4948d341c2cb2992de2d
Douglas Trindade <douglastrindade22@hotmail.com>
2024-08-12 23:38:28 -0300

    feat: booking card

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import { SearchIcon } from "lucide-react";
 import { Header } from "./_components/header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
+import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 const Home = () => {
   return (
@@ -17,6 +21,35 @@ const Home = () => {
             <SearchIcon />
           </Button>
         </div>
+
+        <div className="relative mt-6 w-full h-[150px]">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores FSW Barber"
+            fill
+            className="object-cover rounded-xl"
+          />
+        </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="/avatar.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
